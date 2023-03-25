7c30458c0a0faa264c73bb7b7c769a74747873ed
Bahman Pournazari <bahmanpn@gmail.com>
2023-03-25 22:53:01 +0430

    33-set dynamic url for serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 # from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-
+from drf_dynamic_fields import DynamicFieldsMixin
 from blog.models import Article
 
 
@@ -16,15 +16,16 @@ from blog.models import Article
 #         # return value.first_name+' '+value.last_name
 
 
-class ArticleSerializer(serializers.ModelSerializer):
+class ArticleSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     def get_article_author(self, obj):
         return {
-            'username':obj.author.username,
-            'first_name':obj.author.first_name,
-            'last_name':obj.author.last_name,
+            'username': obj.author.username,
+            'first_name': obj.author.first_name,
+            'last_name': obj.author.last_name,
         }
 
     author = serializers.SerializerMethodField('get_article_author')
+
     # author = serializers.CharField(source="author.username", read_only=True)
     # author = AuthorUserNameField(read_only=True)
     # author = serializers.HyperlinkedIdentityField(view_name='api:authors-detail')
